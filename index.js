5226edc223dad89752a01121d555faacc5aa4846
soumya13042000 <soumyashreebaral086@gmail.com>
2022-11-30 18:48:20 +0530

    done

---
+++ index.js
@@ -0,0 +1,32 @@
+function submitForm(){
+    let date=document.querySelector("#date").value
+    let month=document.querySelector("#month").value
+    let year=document.querySelector("#year").value
+    if(date<1 || date>31){
+        alert("Please Enter correct date")
+    }else if(month<1 || month>12){
+        alert("Please Enter correct month")
+    }else if(year<1000 || year>9999){
+        alert("Please Enter correct year")
+    }else{
+        let cdate=new Date()
+        let d=cdate.getDate()
+        let m=cdate.getMonth()+1
+        let y=cdate.getFullYear()
+        let allmonth=[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        if(date>d){
+            d=d2+month[m-1]
+            m=m-1
+        }
+        if(month>m){
+            m=m+12
+            y=y-1
+        }
+        let d1=d-date
+        let m1=m-month
+        let y1=y-year
+        let p=document.querySelector(".result")
+        p.innerHTML=`Your Age is ${y1} Years ${m1} Months ${d1} Days.`
+    }
+    
+}
